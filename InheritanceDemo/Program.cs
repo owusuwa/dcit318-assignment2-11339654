10a4e0156ff7acd49971e455a67a392cfff5f81e
Nana <nana@gmail.com>
2025-08-05 00:52:02 +0000

    Added Inheritance and Method Overriding demo

---
+++ InheritanceDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Some generic sound");
+    }
+}
+
+class Wolf : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Howl");
+    }
+}
+
+class Koala : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bellow");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal myAnimal = new Animal();
+        Wolf myWolf = new Wolf();
+        Koala myKoala = new Koala();
+
+        myAnimal.MakeSound(); // Output: Some generic sound
+        myWolf.MakeSound();    // Output: Howl
+        myKoala.MakeSound();    // Output: Bellow
+    }
+}
+
