0a9a5586831b74cec24eeabcacdb64e6c13b89a5
Nana <nana@gmail.com>
2025-08-05 01:01:56 +0000

    Added Abstract Classes and Methods demo

---
+++ AbstractClassDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+abstract class Shape
+{
+    public abstract double GetArea();
+}
+
+class Circle : Shape
+{
+    public double Radius { get; set; }
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
+
+class Rectangle : Shape
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public override double GetArea()
+    {
+        return Width * Height;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Circle circle = new Circle(5);
+        Rectangle rectangle = new Rectangle(4, 6);
+
+        Console.WriteLine($"Area of Circle: {circle.GetArea():F2}");
+        Console.WriteLine($"Area of Rectangle: {rectangle.GetArea()}");
+    }
+}
